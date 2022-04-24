f5e0961abfedd891a159b9fedb74a7bf56b0318a
Umer Shahid <55623682+UmerShahidengr@users.noreply.github.com>
2022-04-24 13:51:13 +0500

    Update design_tb.sv

--- design_tb.sv
+++ design_tb.sv
@@ -35,7 +35,7 @@ integer i, fail=0, pass=0, Spass=0, Sfail=0;
 
 initial  
 begin
-
+/*
 // CORNER CASES 0/1
 A = 32'h0;  // 0.0
 B = 32'h1;  // 1.0
@@ -57,7 +57,7 @@ valueB = $bitstoshortreal(B);
 
 
 // CORNER CASES 0/0
-A = 32'h0;  	// 1.0
+A = 32'h0;  	// 0.0
 B = 32'h0;  		// 0.0
 #15
 value =$bitstoshortreal(result);
@@ -76,9 +76,130 @@ valueA = $bitstoshortreal(A);
 valueB = $bitstoshortreal(B);
   $display("Special Case For A = %f and B = %f, expected : %f got : %f",valueA,valueB,valueA/valueB,value);
   
+*/
   
+  
+  
+// SPECIAL TEST PATTERNS
+// CORNER CASES +inf/negative
+A = 32'h7F800000;  // +inf
+B = 32'h85634992;  // any negative
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : -inf got : %f",valueA,valueB,value);
+
+  
+// CORNER CASES 0.0/something
+A = 32'h0;  		// zero
+B = 32'h85634992;  // something
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+
+  
+// CORNER CASES inf/zero
+A = 32'h7F800000;  	// +inf
+B = 32'h0;  		// 0.0
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : inf, got : %f",valueA,valueB,value);
 
 
+// CORNER CASES NaN/something
+A = 32'h7FC00000;  		// NaN
+B = 32'h85634993;  // something
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : nan, got : %f",valueA,valueB,value);
+
+  
+// CORNER CASES subnormal/subnormal
+A = 32'h00005109;  		// subnormal
+B = 32'h80034093;  		// subnormal
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+  
+// CORNER CASES subnormal/normal
+A = 32'h80004093;  		// subnormal
+B = 32'h85634993;  		// normal
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+
+  
+ // CORNER CASES normal/subnormal
+A = 32'h83490200;  		// normal
+B = 32'h80000093;  		// subnormal
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+
+  
+   // CORNER CASES subnormal/normal
+A = 32'h00004101;  		// subnormal
+B = 32'h82340093;  		// normal
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+  
+  
+   // CORNER CASES
+A = 32'h17810000;  		// corner
+B = 32'h622C0000;  		// corner
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+  
+    
+   // CORNER CASES 
+A = 32'h9FDD5C3E;  		// corner
+B = 32'h6A648040;  		// corner
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+
+  
+   // CORNER CASES 
+A = 32'h958E8000;  		// corner
+B = 32'hE0400000;  		// corner
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+
+    
+   // CORNER CASES
+A = 32'h90000000;  		// corner
+B = 32'h4E90001F;  		// corner
+#15
+value =$bitstoshortreal(result);
+valueA = $bitstoshortreal(A);
+valueB = $bitstoshortreal(B);
+  $display("Special Case For A = %f and B = %f, expected : %f, got : %f",valueA,valueB,valueA/valueB,value);
+
+  /*
 // GENRAL CASES
   for(i =0 ; i < 500; i=i+1) begin
 #100
@@ -104,7 +225,7 @@ valueB = $bitstoshortreal(B);
 end
 end	
   $display("No. of Passes = %f and No. of Fails = %f",pass,fail);
-	
+*/	
 end
 
 
