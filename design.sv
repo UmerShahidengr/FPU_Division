8a5b9be67140d7da8d91fd6d1c7980c854a7a279
Umer Shahid <55623682+UmerShahidengr@users.noreply.github.com>
2022-04-25 13:21:03 +0500

    Update design.sv

--- design.sv
+++ design.sv
@@ -18,194 +18,245 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+module fpdiv(
 
+	  output logic     [31:0] z_value_o,
+  	input wire logic [31:0] a_value_i,
+    input wire logic [31:0] b_value_i
+    
 
-module get_sign(A, B, sign); // this is to get the sign of the output
-  input A, B;
-  output sign;
+    );
 
-  xor(sign,A,B);
-endmodule
-
-module division (i_divisor, i_dividend, result, expo, expA, expB);
-  //reg [23:0];
-  input wire [7:0] expA, expB;
-  input [31:0]i_dividend, i_divisor;
-  reg [23:0]quotient=0;
-  reg [7:0]exponent_diff = 8'd0;
-  output reg [7:0]expo=0;
-  reg first_bit = 1'b0;
-  reg done = 1'b0;
-  output reg [22:0]result=0;
-  reg [32:0] dividend=0, divisor=0;
-  integer i;
-  reg[1:0] stateA, next_stateA, stateB, next_stateB;
-  
-  
-  always @ ( i_divisor, i_dividend, expA, expB) begin 
-    dividend = i_dividend;
-    divisor = i_divisor;
-    done = 0;
+    enum {IDLE, UNPACK, SPECIAL_CASES, NORMALIZE_A, NORMALIZE_B, DIVIDE_0, DIVIDE_1, DIVIDE_2, DIVIDE_3, NORMALIZE_0,
+          NORMALIZE_1, ROUND, PACK, DONE} state;
+	integer i=0;
+    logic        [23:0] a_m, b_m, z_m;
+    logic signed [9:0]  a_e, b_e, z_e;
+    logic               a_s, b_s, z_s;
+  logic        [50:0] quotient=0, divisor=0, dividend=0, remainder=0;
+    logic        [5:0]  count;
+   
+    logic guard=0, round_bit=0, sticky=0;
+  always@(a_value_i,b_value_i) begin
+    
+        state = IDLE;
+    	guard=0;
+    round_bit=0;
+    sticky=0;
     quotient=0;
-    exponent_diff = 0;
-    first_bit = 0;
-    stateA=0;
-    stateB=0;
-    next_stateA=0;
-    next_stateB=0;
-        
-    
-    
-    
-    if (divisor[31:0] == 32'b0 && expB[7:0] == 8'b0 && done == 1'b0) begin
-      done = 1'b1;
-      result = 23'b0;
-      expo = 8'b0;
-    end
-    if (dividend[31:0] == 32'b0 && expA[7:0] == 8'b0 && done == 1'b0) begin
-      result = 23'b0;
-      expo = 8'b0;
-      done = 1'b1;
-    end
-    if(done == 1'b0) begin
-      
-      for(i=0; i<32; i=i+1) begin
-      stateA=next_stateA;
-      case (stateA)
-      0: next_stateA=1;
-      1: begin
-      
-      if (quotient[23] != 1'b1) begin
-        next_stateA=1;
-        if (dividend >= divisor) begin
-          dividend = dividend - divisor;    // here we just subtract the divisor from the dividend and shift the dividend left.
-          dividend = dividend << 1;
-          quotient = {quotient[22:0], 1'b1}; // this is to shift the quotient left and add 1 to the lsb.
-          if (first_bit == 1'b0) begin
-            first_bit = 1'b1;
-          end
-        end else begin
-          dividend = dividend << 1;
-          quotient = {quotient[22:0], 1'b0};
-          if (first_bit == 1'b0) begin             //until we get the first bit i.e. the first time the divisor is less than the dividend the exponent value will decrease
-            exponent_diff = exponent_diff + 1;
-          end
-        end
-        end
-      else
-            next_stateA=2;
-      end    
-      2: next_stateA=2;
-      default: next_stateA=0;
-      endcase      
-      end
+    divisor=0;
+    dividend=0;
+    remainder=0;
+    count=0; 
+     
+  for(i=0;i<500;i=i+1) begin
 
-        
-        if (dividend[31:0] == 32'b0 && done == 1'b0) begin
-        for(i=0; i<32; i=i+1) begin
-        stateB=next_stateB;
-        case (stateB)
-        0: next_stateB=1;
-        1: begin
-        
-          if (quotient[23] != 1'b1) begin
-            next_stateB=1;
-            quotient = quotient << 1;
-            if(first_bit == 1'b0) begin           // if we haven got a first_bit by now give output 0.
-              result = 23'b0;
-              expo = 8'b0;
-              done = 1'b1;
-              //break;
+        case (state)
+            IDLE: begin
+                    state = UNPACK;
             end
-          end
-          
-          else
-              next_stateB=2;
-          end    
-          2: next_stateB=2;
-          default: next_stateB=0;
-          endcase      
-         end
-          
-          
-        end
-      
-      
-      
-      end
-      
-      if (done == 1'b0) begin
-        expo = expA - expB +8'd127;
-        //get_exp exp_out (.A(expA), .B(expB), .exp(expo));
-        expo = expo - exponent_diff;
-        if (expo[7] == 0 && expA >= 128 && expB < 128) begin
-          done = 1'b1;
-        end else if (expo[7] == 1 && expA < 128 && expB >= 128) begin
-          done = 1'b1;
-        end
-        if (quotient[23] == 1) begin
-          result = quotient[22:0];
-          done = 1'b1;
-        end
-      end
-    end
-  
 
+            UNPACK: begin
+                a_m   = {1'd0, a_value_i[22:0]};
+                b_m   = {1'd0, b_value_i[22:0]};
+                a_e   = {2'd0, a_value_i[30:23]} - 10'd127;
+                b_e   = {2'd0, b_value_i[30:23]} - 10'd127;
+                a_s   = a_value_i[31];
+                b_s   = b_value_i[31];
+                state = SPECIAL_CASES;
+            end
 
-endmodule // division
+            SPECIAL_CASES: begin
+                // if a is NaN or b is NaN return NaN
+                if ((a_e == 128 && a_m != 0) || (b_e == 128 && b_m != 0)) begin
+                    z_value_o[31]    = 1;
+                    z_value_o[30:23] = 255;
+                    z_value_o[22]    = 1;
+                    z_value_o[21:0]  = 0;
+                    state            = DONE;
+                end
+                // if a is inf and b is inf return NaN
+                else if (a_e == 128 && b_e == 128) begin
+                    z_value_o[31]    = 1;
+                    z_value_o[30:23] = 255;
+                    z_value_o[22]    = 1;
+                    z_value_o[21:0]  = 0;
+                    state            = DONE;
+                end
+                // if a is inf return inf
+                else if (a_e == 128) begin
+                    z_value_o[31]    = a_s ^ b_s;
+                    z_value_o[30:23] = 255;
+                    z_value_o[22:0]  = 0;
+                    // if b is zero but A is not inf return NaN 
+                  if ((b_e == -127) && (b_m == 0) && (a_e != 128)) begin
+                        z_value_o[31]    = 1;
+                        z_value_o[30:23] = 255;
+                        z_value_o[22]    = 1;
+                        z_value_o[21:0]  = 0;
+                    end
+                    state = DONE;
+                end
+                // if b is inf return zero
+                else if (b_e == 128) begin
+                    z_value_o[31]    = a_s ^ b_s;
+                    z_value_o[30:23] = 0;
+                    z_value_o[22:0]  = 0;
+                    state            = DONE;
+                end
+                // if a is zero return zero
+                else if (a_e == -127 && a_m == 0) begin
+                    z_value_o[31]    = a_s ^ b_s;
+                    z_value_o[30:23] = 0;
+                    z_value_o[22:0]  = 0;
+                    // if b is zero return NaN
+                    if (b_e == -127 && b_m == 0) begin
+                        z_value_o[31]    = 1;
+                        z_value_o[30:23] = 255;
+                        z_value_o[22]    = 1;
+                        z_value_o[21:0]  = 0;                        
+                    end
+                    state = DONE;
+                end
+                // if b is zero return inf
+                else if (b_e == -127 && b_m == 0) begin
+                    z_value_o[31]    = a_s ^ b_s;
+                    z_value_o[30:23] = 255;
+                    z_value_o[22:0]  = 0;
+                    state    = DONE;
+                end else begin
+                    // denormalized number
+                    if (a_e == -127) begin
+                        a_e = -126;
+                    end else begin
+                        a_m[23] = 1;
+                    end
+                    // denormalized number
+                    if (b_e == -127) begin
+                        b_e = -126;
+                    end else begin
+                        b_m[23] = 1;
+                    end
+                    state = NORMALIZE_A;
+                end
+            end
 
-//This module is the main module where all the sub modules will be included
+            NORMALIZE_A: begin
+                if (a_m[23]) begin
+                    state = NORMALIZE_B;
+                end else begin
+                    a_m = a_m << 1;
+                    a_e = a_e - 1;
+                end
+            end
 
-module fpdiv(AbyB, InputA, InputB);
-input [31:0] InputA, InputB;
-output [31:0] AbyB;
-wire [7:0]  expAbyB;
-wire [22:0]  mantAbyB;
-wire  signAbyB;
-wire [32:0] temp_divisor, temp_dividend;
-wire case0, casePinf, caseNinf, caseNaN;
+            NORMALIZE_B: begin
+                if (b_m[23]) begin
+                    state = DIVIDE_0;
+                end else begin
+                    b_m = b_m << 1;
+                    b_e = b_e - 1;
+                end
+            end
 
-// Special Case if B=0 & A is any positive number; (answer=+inf)
-// Special Case if A=+inf & B is any positive number; (answer=+inf)  
-  assign casePinf = (InputB==32'h00000000 && InputA[31]==0) | 
-    				(InputA==32'h7F800000 && InputB[31]==0) ? 1 : 0;
-         
-// Special Case if B=0 & A is any negative number; (answer=-inf)
-  // Special Case if A=+inf & B is any positive number; (answer=-inf)  
-  assign caseNinf = (InputB==32'h00000000 & InputA[31]==1) |
-    				(InputA==32'h7F800000 && InputB[31]==1) ? 1 : 0;
+            DIVIDE_0: begin
+                z_s       = a_s ^ b_s;
+                z_e       = a_e - b_e;
+                quotient  = 0;
+                remainder = 0;
+                count     = 0;
+                dividend  = {a_m, 27'd0};
+                divisor   = {27'd0, b_m};
+                state     = DIVIDE_1;            
+            end
 
-// Special Case if B=0 & A=0; (answer=NaN)
-  assign caseNaN = (InputB[30:0]==0 & InputA[30:0]==0) |
-    (InputA[30:23]==8'hFF & InputA!=32'h7F800000) | 
-    (InputB[30:23]==8'hFF & InputB!=32'h7F800000) ? 1 : 0;
+            DIVIDE_1: begin
+                quotient     = quotient << 1;
+                remainder    = remainder << 1;
+                remainder[0] = dividend[50];
+                dividend     = dividend << 1;
+                state        = DIVIDE_2;
+            end
 
-// Special Case if B=+inf & A is any positive number; (answer=+0.0)
-// Special Case if B=-inf & A is any negative number; (answer=0.0)
-// Special Case if A=0; (answer=0.0)
-  
-  assign case0 = (InputA==32'h0) | (InputA==32'h80000000) | (InputB==32'h7F800000) | (InputB==32'hFF800000) ? 1 : 0;
+            DIVIDE_2: begin
+                if (remainder >= divisor) begin
+                    quotient[0] = 1;
+                    remainder   = remainder - divisor;
+                end
+                if (count == 49) begin
+                    state = DIVIDE_3;
+                end else begin
+                    count = count + 1;
+                    state = DIVIDE_1;
+                end
+            end
 
+            DIVIDE_3: begin
+                z_m       = quotient[26:3];
+                guard     = quotient[2];
+                round_bit = quotient[1];
+                sticky    = quotient[0] | (remainder != 0);
+                state     = NORMALIZE_0;
+            end
 
+            NORMALIZE_0: begin
+                if (z_m[23] == 0 && z_e > -126) begin
+                    z_e       = z_e - 1;
+                    z_m       = z_m << 1;
+                    z_m[0]    = guard;
+                    guard     = round_bit;
+                    round_bit = 0;
+                end else begin
+                    state     = NORMALIZE_1;
+                end
+            end
 
+            NORMALIZE_1: begin
+                if (z_e < -126) begin
+                    z_e       = z_e + 1;
+                    z_m       = z_m >> 1;
+                    guard     = z_m[0];
+                    round_bit = guard;
+                    sticky    = sticky | round_bit;
+                end else begin
+                    state     = ROUND;
+                end
+            end
 
-assign temp_divisor = {1'b1,InputB[23:0],7'd0};
-assign temp_dividend = {1'b1,InputA[23:0],7'd0};
-get_sign s_out (.A(InputA[31]), .B(InputB[31]), .sign(signAbyB));
-division divide (.i_divisor({1'b1,InputB[22:0],8'd0}), 
-                 .i_dividend({1'b1,InputA[22:0],8'd0}), 
-                 .result(mantAbyB), 
-                 .expo(expAbyB), 
-                 .expA(InputA[30:23]), .expB(InputB[30:23]));
-                 
-                    // ZERO = 32'h0;
-                    // Pinf = 32'h7F800000
-                    // Ninf = 32'hFF800000
-                    // NaN = 32'hFFFFFFFFF
-                 
-                 // GENERAL CASE
-assign AbyB = caseNaN ? 32'h7FFFFFF0 : case0 ? 0 :  casePinf ? 32'h7F800000 : caseNinf ? 32'hFF800000 : {signAbyB,expAbyB,mantAbyB};
+            ROUND: begin
+                if (guard && (round_bit | sticky | z_m[0])) begin
+                    z_m = z_m + 1;
+                    if (z_m == 24'hffffff) begin
+                        z_e = z_e + 1;
+                    end
+                end
+                state = PACK;
+            end
 
-//assign AbyB = {signAbyB,expAbyB,mantAbyB};
+            PACK: begin
+                z_value_o[22:0]  = z_m[22:0];
+                z_value_o[30:23] = z_e[7:0] + 127;
+                z_value_o[31]    = z_s;
+                if (z_e == -126 && z_m[23] == 0) begin
+                    z_value_o[30:23] = 0;
+                end
+                // if overflow occurs, return inf
+                if (z_e > 127) begin
+                    z_value_o[22:0]  = 0;
+                    z_value_o[30:23] = 255;
+                    z_value_o[31]    = z_s;
+                end
+                state = DONE;
+            end
 
+            DONE: begin
+                state         = DONE;
+                i = 500;
+            end
+
+        endcase
+       
+            end
+  end
 endmodule
+
